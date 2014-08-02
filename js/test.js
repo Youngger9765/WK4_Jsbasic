164dcf2d8e6c684a9042fa57e857814c3463baa9
yungger9765 <purpleice9765@msn.com>
2014-08-02 12:57:49 +0800

    jsbasic

---
+++ js/test.js
@@ -0,0 +1,50 @@
+<<<<<<< HEAD
+var firstVar = {
+		name: "123",
+		fs: funciton(data){
+			console.log("i am function");
+			}
+		};
+		
+		console.log(firstVar.name);
+		firstVar.fs();
+		
+
+	
+function firstfunc(data){
+			this.name = "i am ian, brovo!!";
+		}
+		
+var firstfun1 = new firstfunc();
+		console.log(firstfunc1.name);
+		
+
+var glasses = new function(){
+		this.size=20;
+		}
+=======
+var firstVar = {
+		name: "123",
+		fs: funciton(data){
+			console.log("i am function");
+			}
+		};
+		
+		console.log(firstVar.name);
+		firstVar.fs();
+		
+
+	
+function firstfunc(data){
+			this.name = "i am ian, brovo!!";
+		}
+		
+var firstfun1 = new firstfunc();
+		console.log(firstfunc1.name);
+		
+
+var glasses = new function(){
+		this.size=20;
+		}
+>>>>>>> 943763c3709c41ce8287cc51cb0bcee742cd81a9
+		console.log(glasses.size);
